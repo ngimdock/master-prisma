4b546222dd867c948dc003134afe3d258e974ca7
ngimdock <ngimdockzemfack@gmail.com>
2023-02-20 09:43:33 +0100

    Update Readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 # Master prisma
+
+Learn how to use prisma modern typescript ORM to map data in a postgres database.
--- server.ts
+++ server.ts
@@ -1,19 +1,6 @@
 import { PrismaClient, Role, User } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const USERS = [
-  {
-    email: "1@gmail.com",
-    name: "1",
-    age: 20,
-  },
-  {
-    email: "2@gmail.com",
-    name: "2",
-    age: 21,
-  },
-];
-
 async function main() {
   const user = await prisma.user.update({
     where: {
